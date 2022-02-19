fc480fc7e706f2574685c244d05a5df90b025620
Bevz Aleksandr <as-bivz@yandex.ru>
2022-02-19 16:57:05 +0300

    Move a definition of factory method
    Moving from FactoryApplication to MethodFactoryApplication.

--- src/main/java/ru/bevz/study/design_patterns/factory/method/MethodFactoryApplication.java
+++ src/main/java/ru/bevz/study/design_patterns/factory/method/MethodFactoryApplication.java
@@ -1,5 +1,11 @@
 package ru.bevz.study.design_patterns.factory.method;
 
+/**
+ * <h1>Фабричный метод</h1>
+ * Паттерн Фабричный Метод определяет интерфейс создания обеъекта,
+ * но позволяет субклассам выбрать класс создаваемого экземпляра.
+ * Таким образом, Фабричный Метод делегирует операцию создания экземпляра субклассам.
+ */
 public class MethodFactoryApplication {
     public static void main(String[] args) {
         PizzaStore pizzaStore;
